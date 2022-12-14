a0d56580895f622b68fe4dcee5a4e067faf25dfe
sadtrick <sadtrick@pm.me>
2022-12-14 23:29:53 +0100

    chore: added tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
-	site: 'https://sadtrick@github.io',
-	base: '/fake-scratchcard-lottery',
-	integrations: [mdx(), sitemap({
-		filter: (page) => !page.match("\/lottery\/([a-zA-Z]+)")
-	})],
+  site: 'https://sadtrick@github.io',
+  base: '/fake-scratchcard-lottery',
+  integrations: [mdx(), sitemap({
+    filter: page => !page.match("\/lottery\/([a-zA-Z]+)")
+  }), tailwind()]
 });
